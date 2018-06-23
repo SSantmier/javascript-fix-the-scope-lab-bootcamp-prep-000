cec6fe812c19455d23349221800f7ec1f19ed0ec
Sara S <ssantmier@gmail.com>
2018-06-23 18:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function myAnimal() {return animal}
 
 function yourAnimal() {var animal = 'cat'; return animal;}
 
-function add2(n) {return n + two const two = 2}
+function add2(n) {return n += 2}
 
 var funkyFunction = function() {
   return function() {
